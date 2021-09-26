6b50f3c85fa28aedd5d585988b1d2bd533e4cfca
BinRui0222 <s1410922042@gms.nutc.edu.tw>
2021-09-26 18:07:57 +0800

    20210926

--- a.js
+++ a.js
@@ -1,5 +1,15 @@
 const readline = require('readline-sync');
-w = readline.question('體重(kg)');
-h = readline.question('身高(cm)');
+w = readline.question('Weight(kg)');
+h = readline.question('Height(cm)');
+if (w > 635 | w < 5 |h > 272 | h< 56){
+    console.log("數值錯誤");
+    return 0 ;
+}
 var bmi =w/((h/100)**2);
 console.log("BMI = "+ bmi);
+if (bmi < 18.5) {
+    console.log("過輕");
+}
+if (bmi > 24){
+    console.log("過重");
+}
