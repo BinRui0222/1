547227c84cc398c9888da41eb2599359bd8e6a38
BinRui0222 <s1410922042@gms.nutc.edu.tw>
2021-09-29 16:05:30 +0800

    2021

--- b.js
+++ b.js
@@ -1,6 +1,17 @@
 const readline = require('readline-sync');
 var t = 10,b = 1,n = 7,Target;
 while(true){
-    Target=readline.question('Now'+ n + 'F');
+    Target=readline.question('目前在'+ n + 'F,要去哪樓?');
     Target=parseInt(Target);
+    if(isNaN(Target) || Target > t || Target < b){
+        console.log('Wrong,輸入1~10整數');
+        continue;
+    }
+    if(Target == n){
+        console.log("Leave");
+        break;
+    }
+    else{
+        console.log('continue');
+    }
 }
