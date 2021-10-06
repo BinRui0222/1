71ccee44943d002ef0638c3f38d8a4e181642b78
BinRui0222 <s1410922042@gms.nutc.edu.tw>
2021-10-06 16:10:11 +0800

    1

--- b3.js
+++ b3.js
@@ -3,9 +3,11 @@ var top = 10,bott = 0,now = 3,Target;
 var floor = ["B3","B2","B1","1","2","3","4","5","6","7","8"];
 while(true){
     Target=readline.question("目前在"+ floor[now] + "F,要去哪樓?");
+    Target=Target.toUpperCase();
     Target=floor.indexOf(Target);
     if(Target<0){
-        console.log("Wrong,輸入"+floor[bott]+"~"+floor[top]+"樓層");
+        console.log("Wrong,輸入"+floor.toString()+"樓層");
+        continue;
     }
     if(Target == now){
         console.log("Leave");
@@ -14,7 +16,7 @@ while(true){
     else{
         if(Target < now){
             console.log("電梯下樓");
-            for(now == now;Target < now;now --){
+            for(now = now;Target < now;now --){
                 console.log("電梯在" +floor[now]+ "樓");
             }
             console.log(floor[now]);
