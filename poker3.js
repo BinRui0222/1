8e774cb6a2edd34c386c21d26ac9fee42b2a8f1c
rr <sss@gmail.com>
2021-10-20 14:06:51 +0800

    2

--- poker3.js
+++ poker3.js
@@ -2,29 +2,28 @@ var p =[],i;
 for(i=0;i<52;i++){
     switch(parseInt(i/13)){
         case 0:
-            p.push("H"+(parseInt(i%13)+1));
+            p.push("H"+(parseInt(i%13)+1) + " ");
             break;
         case 1:
-            p.push("B"+(parseInt(i%13)+1));
+            p.push("B"+(parseInt(i%13)+1) + " ");
             break;
         case 2:
-            p.push("L"+(parseInt(i%13)+1));
+            p.push("L"+(parseInt(i%13)+1) + " ");
             break;
         case 3:
-            p.push("M"+(parseInt(i%13)+1));
+            p.push("M"+(parseInt(i%13)+1) + " ");
             break;
     }
 }
-console.log(p.toString());
 var p1=[];p2=[];p3=[];p4=[];
 function compare(a,b){
     return Math.random()-0.5;
 }
 function compare2(a,b){
-    if(b.charCodeAt(1) == a.charCodeAt(1)){
+    if(b.charCodeAt(2) == a.charCodeAt(2)){
     }
     else{
-        return b.charCodeAt(1) - a.charCodeAt(1);
+        return b.charCodeAt(2) - a.charCodeAt(2);
     }
 }
 function compare3(a,b){
@@ -35,10 +34,10 @@ function compare3(a,b){
     }
 }
 function compare1(a,b){
-    if(b.charCodeAt(2) == a.charCodeAt(2)){
+    if(b.charCodeAt(1) == a.charCodeAt(1)){
     }
     else{
-        return b.charCodeAt(2) - a.charCodeAt(2);
+        return b.charCodeAt(1) - a.charCodeAt(1);
     }
 }
 p.sort(compare1);
